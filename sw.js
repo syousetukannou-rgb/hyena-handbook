a40e323599f40d736e665052af04a7c3ecf8982d
hyena-app <hyena@local>
2026-07-11 19:17:20 +0900

    計算機に会員モードを追加（正規ラインを秘密Gistから読込・公開コードに数値なし）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hyena-handbook-v8";
+const CACHE = "hyena-handbook-v9";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
