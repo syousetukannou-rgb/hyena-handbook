dac52aa62a525cda0af1fac3911670708b1c3201
hyena-app <hyena@local>
2026-07-11 18:48:25 +0900

    会員メモタブを追加（秘密Gistから読込・公開リポジトリに内容は含まない）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hyena-handbook-v7";
+const CACHE = "hyena-handbook-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
