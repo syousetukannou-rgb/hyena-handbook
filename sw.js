60bb9540c5e2cd2b297f8ed3eb5c17b38321da2a
hyena-app <hyena@local>
2026-07-12 22:30:59 +0900

    秘宝伝・真打吉宗ページを追加（17機種、正規ラインは会員メモ参照）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hyena-handbook-v13";
+const CACHE = "hyena-handbook-v14";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
